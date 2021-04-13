ebd20fc6fa7e98c5684b8c26fe24a09cfc84642f
bkf2020 <74793713+bkf2020@users.noreply.github.com>
2021-04-13 11:32:56 -0700

    updates to add problemsets into scraper.py

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -69,6 +69,7 @@ for img in soup.find_all(name="img"):
 			continue
 
 if(test_name == "AMC8"):
+	copyfile("problemsetamc.html", year_str + "/problemset.html")
 	skip = input("How many rows before the problems should be skipped? ")
 	while (not is_int(skip)):
 		print("Invalid number!")
@@ -117,7 +118,8 @@ elif(test_name == "AMC10" or test_name == "AMC12"):
 		os.mkdir("./" + year_str + "/B")
 	except:
 		print("directories already exist. that's okay")
-
+	copyfile("problemsetamc.html", year_str + "/A/problemset.html")
+	copyfile("problemsetamc.html", year_str + "/B/problemset.html")
 	skip = input("How many rows before test A should be skipped? ")
 	while (not is_int(skip)):
 		print("Invalid number!")
@@ -214,7 +216,9 @@ elif(test_name == "AIME"):
 		os.mkdir("./" + year_str + "/II")
 	except:
 		print("directories already exist. that's okay")
-
+	copyfile("problemsetaime.html", year_str + "/I/problemset.html")
+	copyfile("problemsetaime.html", year_str + "/II/problemset.html")
+	
 	skip = input("How many rows before test I should be skipped? ")
 	while (not is_int(skip)):
 		print("Invalid number!")
